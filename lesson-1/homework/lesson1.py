0c4024bd2a5c50589ddc2748f7196fe27ed8bf78
DildoraAzamova <dildoraa56@gmail.com>
2025-05-02 12:10:54 +0500

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,30 @@
+# 1
+side = float(5)
+perimetr = 4*side
+area = side*side
+print(f"Perimetr of the square: {perimetr:}")
+print(f"Area of the square:{area}")
+
+# 2
+import math
+DiametrOfCicle = 10
+lenght = math.pi*DiametrOfCicle
+print(f'Length of cicle: {lenght}')
+
+# # 3
+a=3
+b=6
+mean=(a+b)/2
+print(f'The mean: {mean}')
+
+# 4
+a=4
+b=8
+sum=a+b
+product=a*b
+squareA=a**(1/2)
+squareB=b**(1/2)
+print(f'sum of numbers:{sum}')
+print(f'product of numbers:{product}')
+print(f'square of a:{squareA}')
+print(f'square of a:{squareB}')
